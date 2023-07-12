6c094e1a2385391f9b95923816a6e5c2f944700c
Nathan Errington <nathan.guy59@gmail.com>
2023-07-12 12:34:44 +0100

    Added basic versions of the Track, Album, Artist and Playlist classes, the ability to view them in the window and switch views

--- Demo/MediaPlayerDemo/MediaPlayerDemo/MainWindow.xaml.cs
+++ Demo/MediaPlayerDemo/MediaPlayerDemo/MainWindow.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using MediaPlayerDemo.MediaClass;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Track = MediaPlayerDemo.MediaClass.Track;
 
 namespace MediaPlayerDemo
 {
@@ -20,9 +25,97 @@ namespace MediaPlayerDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Track> Tracks = new ObservableCollection<Track>();
+        ObservableCollection<Album> Albums = new ObservableCollection<Album>();
+        ObservableCollection<Artist> Artists = new ObservableCollection<Artist>();
+        ObservableCollection<Playlist> Playlists = new ObservableCollection<Playlist>();
+
+           
         public MainWindow()
         {
             InitializeComponent();
+
+            Tracks.Add(new Track("The Whirlwind", 5, "V. Out Of The Night", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 6, "VI. Rose Colored Glasses", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 7, "VII. Evermore", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 1, "I. Overture [Instrumental] / Whirlwind", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 2, "II.The Wind Blew Them All Away", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 3, "III. On The Prowl", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 4, "IV. A Man Can Feel", "Transatlantic"));
+            Tracks.Add(new Track("Octavarium", 4, "I Walk Beside You", "Dream Theater"));
+            Tracks.Add(new Track("Octavarium", 5, "Panic Attack", "Dream Theater"));
+            Tracks.Add(new Track("Octavarium", 6, "Never Enough", "Dream Theater"));
+            Tracks.Add(new Track("Octavarium", 7, "Sacrificed Sons", "Dream Theater"));
+            Tracks.Add(new Track("The Whirlwind", 8, "VIII. Set Us Free", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 9, "IX. Lay Down Your Life", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 10, "X. Pieces Of Heaven [Instrumental]", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 11, "XI. Is It Really Happening?", "Transatlantic"));
+            Tracks.Add(new Track("The Whirlwind", 12, "XII. Dancing With Eternal Glory / Whirwind (Reprise)", "Transatlantic"));
+            Tracks.Add(new Track("Octavarium", 1, "The Root Of All Evil", "Dream Theater"));
+            Tracks.Add(new Track("Octavarium", 2, "The Answer Lies Within", "Dream Theater"));
+            Tracks.Add(new Track("Octavarium", 3, "These Walls", "Dream Theater"));
+            Tracks.Add(new Track("Octavarium", 8, "Octavarium", "Dream Theater"));
+
+
+
+            Albums.Add(new Album("The Whirlwind"));
+            Albums.Add(new Album("Octavarium"));
+
+            Artists.Add(new Artist("Transatlantic"));
+            Artists.Add(new Artist("Dream Theater"));
+
+            Playlists.Add(new Playlist("playlist 1"));
+            Playlists.Add(new Playlist("new playlist"));
+
+
+            lbPlaylists.ItemsSource = Playlists;
+            lvSongs.ItemsSource = Tracks;
+            lvAlbums.ItemsSource = Albums;
+            lvArtists.ItemsSource = Artists;
+            lvPlaylists.ItemsSource = Playlists;
+
+        }
+
+        private void btnSongs_Click(object sender, RoutedEventArgs e)
+        {
+            lvAlbums.Visibility = Visibility.Collapsed;
+            lvArtists.Visibility = Visibility.Collapsed;
+            lvPlaylists.Visibility = Visibility.Collapsed;
+            lvSongs.Visibility = Visibility.Visible;
+        }
+
+        private void btnAlbums_Click(object sender, RoutedEventArgs e)
+        {
+            lvSongs.Visibility = Visibility.Collapsed;
+            lvArtists.Visibility = Visibility.Collapsed;
+            lvPlaylists.Visibility = Visibility.Collapsed;
+            lvAlbums.Visibility = Visibility.Visible;
+        }
+
+        private void btnArtists_Click(object sender, RoutedEventArgs e)
+        {
+            lvSongs.Visibility = Visibility.Collapsed;
+            lvAlbums.Visibility = Visibility.Collapsed;
+            lvPlaylists.Visibility = Visibility.Collapsed;
+            lvArtists.Visibility = Visibility.Visible;
+        }
+
+        private void btnPlaylists_Click(object sender, RoutedEventArgs e)
+        {
+            lvSongs.Visibility = Visibility.Collapsed;
+            lvAlbums.Visibility = Visibility.Collapsed;
+            lvArtists.Visibility = Visibility.Collapsed;
+            lvPlaylists.Visibility = Visibility.Visible;
+        }
+
+        private void btnAlbum_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnArtist_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Demo/MediaPlayerDemo/MediaPlayerDemo/MediaClasses.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayerDemo.MediaClass
+{
+    public class Track
+    {
+        public string Title { get; set; }
+        public int SongNo { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+
+        public Track(string title, int songNo, string album, string artist) { 
+            Title = title;
+            SongNo = songNo;
+            Album = album;
+            Artist = artist;
+        }
+    }
+
+    public class Album
+    {
+        public string Name { get; set; }
+
+        public Album(string name)
+        {
+            Name = name;
+        }
+       
+    }
+
+    public class Artist
+    {
+        public string Name { get; set; }
+
+        public Artist(string name)
+        {
+            Name = name;
+        }
+
+    }
+
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public Playlist(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
