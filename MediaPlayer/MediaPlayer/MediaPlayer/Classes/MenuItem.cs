a185a5e9092a8402c5a24e2c84f8e4cea50265a2
Nathan Errington <nathan.guy59@gmail.com>
2023-07-26 14:38:07 +0100

    Created basic page navigation

---
+++ MediaPlayer/MediaPlayer/MediaPlayer/Classes/MenuItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MediaPlayer.Classes
+{
+    /// <summary>
+    /// Heirarchical class structure for populating a <see cref="TreeView"/>
+    /// </summary>
+    public class MenuItem
+    {
+        /// <summary>
+        /// Name of the navigation option - what is seen on trNav
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Which <see cref="Page"/> to be opened by this <see cref="MenuItem">MenuItem</see> 
+        /// </summary>
+        public Type PageType { get; set; }
+
+        /// <summary>
+        /// <see cref="ObservableCollection{T}"/> of this <see cref="MenuItem">MenuItem's</see> children
+        /// </summary>
+        public ObservableCollection<MenuItem> Children { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuItem"/> class with an empty <see cref="ObservableCollection{T}"/> of children
+        /// </summary>
+        /// <param name="title">The <see cref="Title">Title</see> of this instance of <see cref="MenuItem"/></param>
+        /// <param name="pageType">The <see cref="PageType">Title</see> of this instance of <see cref="MenuItem"/></param>
+        public MenuItem(string title, Type pageType)
+        {
+            Title = title;
+            PageType = pageType;
+            this.Children = new ObservableCollection<MenuItem>();
+        }
+    }
+}
--- MediaPlayer/MediaPlayer/MediaPlayer/Windows/MainWindow.xaml.cs
+++ MediaPlayer/MediaPlayer/MediaPlayer/Windows/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MediaPlayer.Classes;
+using MediaPlayer.Pages;
+using MenuItem = MediaPlayer.Classes.MenuItem;
 
 namespace MediaPlayer.Windows
 {
@@ -20,9 +24,43 @@ namespace MediaPlayer.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// <see cref="ObservableCollection{T}"/> of <see cref="MenuItem">MenuItems</see> for populating <see cref="tvNav">tvNav</see> on the <see cref="MainWindow"/>
+        /// </summary>
+        ObservableCollection<MenuItem> NavItems;
+
         public MainWindow()
         {
             InitializeComponent();
+            CreateNavItems();
+            tvNav.ItemsSource = NavItems;
+        }
+
+        /// <summary>
+        /// Method for populating <see cref="NavItems"/>
+        /// </summary>
+        private void CreateNavItems()
+        {
+            NavItems = new ObservableCollection<MenuItem>();
+            MenuItem mniMusic = new MenuItem("Music", typeof(TracksPage));
+            mniMusic.Children.Add(new MenuItem("Album", typeof(AlbumsPage)));
+            mniMusic.Children.Add(new MenuItem("Artist", typeof(ArtistsPage)));
+            mniMusic.Children.Add(new MenuItem("Genre", typeof(GenresPage)));
+            mniMusic.Children.Add(new MenuItem("Year", typeof(YearsPage)));
+            MenuItem mniPlaylists = new MenuItem("Playlists", typeof(PlaylistsPage));
+            mniPlaylists.Children.Add(new MenuItem("Playlist1", typeof(SinglePlaylistPage)));
+            mniPlaylists.Children.Add(new MenuItem("New Playlist", typeof(SinglePlaylistPage)));
+            NavItems.Add(mniMusic);
+            NavItems.Add(mniPlaylists);
+        }
+
+        private void tvNav_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(tvNav.SelectedItem != null)
+            {
+                Type pageType = ((MenuItem)tvNav.SelectedItem).PageType;
+                MainView.Content = (Page)Activator.CreateInstance(pageType);
+            }
         }
     }
 }
