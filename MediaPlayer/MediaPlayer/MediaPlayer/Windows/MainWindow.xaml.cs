c938ebe5ff7399f0883de84b9b5cec775a0a96ea
Nathan Errington <nathan.guy59@gmail.com>
2023-08-03 16:32:06 +0100

    Modified project file to enable access to Windows Runtime APIs, so as to use the Windos.Storage.FileProperties.MusicProperties class and other relevant classes

--- MediaPlayer/MediaPlayer/MediaPlayer/Windows/MainWindow.xaml.cs
+++ MediaPlayer/MediaPlayer/MediaPlayer/Windows/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using MediaPlayer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using Windows.Storage;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +17,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Media.Control;
+using Windows.Storage.FileProperties;
 
 namespace MediaPlayer.Windows
 {
@@ -20,9 +27,31 @@ namespace MediaPlayer.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MediaDBContext _context = new MediaDBContext();
+
+        private CollectionViewSource artistsViewSource;
         public MainWindow()
         {
             InitializeComponent();
+            artistsViewSource = (CollectionViewSource)FindResource(nameof(artistsViewSource));
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _context.Artists.Load();
+            artistsViewSource.Source = _context.Artists.Local.ToObservableCollection();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _context.SaveChanges();
+            dgArtists.Items.Refresh();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _context.Dispose();
+            base.OnClosing(e);
         }
     }
 }
